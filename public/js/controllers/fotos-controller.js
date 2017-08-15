883112df643bf996205f49530d0b56d582188fde
Monique Azevedo <moniqueaz.si@gmail.com>
2017-08-15 14:01:43 -0300

    Injecao de dependencias, comunicacao com o back-end para trazer as fotos dinamicamentes / Melhora do layout com bootstrap

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,8 +1,32 @@
-angular.module('alurapic').controller('FotosController', function($scope) {
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
+
+    $scope.fotos = [];
+   
+    $http.get('/v1/fotos')
+    .success(function(retorno){
+        console.log(retorno);
+        $scope.fotos = retorno; // nao precisa fazer o retorno.data
+    })
+    .error(function(erro){
+        console.log(erro);
+    });
     
+
+    
+
+    /*
+    var promise = $http.get('/v1/fotos');
+    promise.then(function(retorno){
+        $scope.fotos = retorno.data;
+    }).catch(function(error){
+        console.log(error);
+    });
+    */
+
+    /*
         $scope.foto = {
             titulo : 'Leão',
             url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
         };
-    
+    */
     });

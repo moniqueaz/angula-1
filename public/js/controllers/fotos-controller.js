3b043366a612e15677a2f68403d8a4805a8b30e0
Monique Azevedo <moniqueaz.si@gmail.com>
2017-08-16 16:15:33 -0300

    Alteracao do controlle para uso do ngResource

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,9 +1,15 @@
-angular.module('alurapic').controller('FotosController', function($scope, $http) {
+angular.module('alurapic').controller('FotosController', function($scope, recursoFoto) {
 
     $scope.fotos = [];
     $scope.filtro = '';
     $scope.mensagem = '';
-   
+
+    recursoFoto.query(function(fotos){
+        $scope.fotos = fotos;
+    }, function(erro){
+        console.log(erro);
+    });
+   /*
     $http.get('/v1/fotos')
     .success(function(retorno){
         console.log(retorno);
@@ -12,9 +18,21 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
     .error(function(erro){
         console.log(erro);
     });
+   */
+
 
     $scope.remover = function(foto){
 
+              // novidade aqui!
+              recursoFoto.delete({fotoId: foto._id}, function() {
+                var indiceDaFoto = $scope.fotos.indexOf(foto);
+                $scope.fotos.splice(indiceDaFoto, 1);
+                $scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso!';
+            }, function(erro) {
+                console.log(erro);
+                $scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
+            });
+        /*
         $http.delete('/v1/fotos/' + foto._id)
         .success(function(){
             // javascript
@@ -25,6 +43,7 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
         .error(function(erro){
             $scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
         });
+        */
     };
 
 

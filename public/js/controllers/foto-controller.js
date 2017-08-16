eb309ac156f442f77a681f7f8f3de24da9cdf7fb
Monique Azevedo <moniqueaz.si@gmail.com>
2017-08-16 11:12:42 -0300

    Inclusao das funcionalidades de edicao e remocao

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,19 +1,41 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http){
+angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams){
 
     $scope.foto = {};
     $scope.mensagem = '';
 
+    if($routeParams.fotoId){
+        // buscar a foto no servidor
+        $http.get('v1/fotos/' + $routeParams.fotoId)
+        .success(function(foto){
+            $scope.foto = foto;
+        })
+        .error(function(erro){
+            console.log(erro);
+            $scope.mensagem = 'Não foi possível obter a foto';
+        });
+    }
+
     $scope.submeter = function(){
         if($scope.formulario.$valid){
-
-            $http.post('/v1/fotos', $scope.foto)
-            .success(function(){
-                $scope.foto = {};
-                $scope.mensagem = 'Foto adicionada com sucesso';
-            })
-            .error(function(erro){
-                $scope.mensagem = 'Não foi possível cadastrar a foto';
-            });
+            if($routeParams.fotoId){
+                $http.put('/v1/fotos/' + $scope.foto._id, $scope.foto)
+                .success(function(){
+                    $scope.mensagem = 'Foto ' + $scope.foto.titulo + ' foi alterada';
+                })
+                .error(function(erro){
+                    console.log(erro);
+                    $scope.mensagem = 'Não foi possível alterar a foto ' + $scope.foto.titulo;
+                });
+            }else{
+                $http.post('/v1/fotos', $scope.foto)
+                .success(function(){
+                    $scope.foto = {};
+                    $scope.mensagem = 'Foto adicionada com sucesso';
+                })
+                .error(function(erro){
+                    $scope.mensagem = 'Não foi possível cadastrar a foto';
+                });
+            }
         }
     };
 });

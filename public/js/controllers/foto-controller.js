19895d4963eee3a1ac27d86cf2f23935daeeb287
Monique Azevedo <moniqueaz.si@gmail.com>
2017-08-17 14:44:08 -0300

    Inclusao de um novo servico de inclusao e edicao, refatorando o foto-controller para trabalhar com esse novo servico

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,4 +1,5 @@
-angular.module('alurapic').controller('FotoController', function($scope, recursoFoto, $routeParams){
+angular.module('alurapic')
+    .controller('FotoController', function($scope, recursoFoto, $routeParams, cadastroDeFotos){
 
     $scope.foto = {};
     $scope.mensagem = '';
@@ -11,58 +12,19 @@ angular.module('alurapic').controller('FotoController', function($scope, recurso
             console.log(erro);
             $scope.mensagem = 'Não foi possível obter a foto';
         });
-
-        /*
-        // buscar a foto no servidor
-        $http.get('v1/fotos/' + $routeParams.fotoId)
-        .success(function(foto){
-            $scope.foto = foto;
-        })
-        .error(function(erro){
-            console.log(erro);
-            $scope.mensagem = 'Não foi possível obter a foto';
-        });
-        */
     }
 
     $scope.submeter = function(){
         if($scope.formulario.$valid){
-            if($routeParams.fotoId){
-                recursoFoto.update({fotoId: $scope.foto._id}, $scope.foto, function(){
-                        $scope.mensagem = 'Foto ' + $scope.foto.titulo + ' foi alterada';
-                    }, function(erro){
-                        console.log(erro);
-                        $scope.mensagem = 'Não foi possível alterar a foto ' + $scope.foto.titulo;
-                    });
-                /*
-                $http.put('/v1/fotos/' + $scope.foto._id, $scope.foto)
-                .success(function(){
-                    $scope.mensagem = 'Foto ' + $scope.foto.titulo + ' foi alterada';
-                })
-                .error(function(erro){
-                    console.log(erro);
-                    $scope.mensagem = 'Não foi possível alterar a foto ' + $scope.foto.titulo;
-                });
-                */
-            }else{
-                recursoFoto.save($scope.foto, function(){
-                    $scope.foto = {};
-                    $scope.mensagem = 'Foto adicionada com sucesso';
-                }, function(erro){
-                    console.log(erro);
-                    $scope.mensagem = 'Não foi possível cadastrar a foto';
-                });
-                /*
-                $http.post('/v1/fotos', $scope.foto)
-                .success(function(){
-                    $scope.foto = {};
-                    $scope.mensagem = 'Foto adicionada com sucesso';
-                })
-                .error(function(erro){
-                    $scope.mensagem = 'Não foi possível cadastrar a foto';
-                });
-                */
-            }
+
+            cadastroDeFotos.cadastrar($scope.foto)
+            .then(function(dados){
+                $scope.mensagem = dados.mensagem;
+                if(dados.inclusao) $scope.foto = {};
+            })
+            .catch(function(erro){
+                $scope.mensagem = erro.mensagem;
+            });
         }
     };
 });
--- public/js/services/meus-servicos.js
+++ public/js/services/meus-servicos.js
@@ -5,4 +5,42 @@ angular.module('meusServicos', ['ngResource'])
             method: 'PUT'
         }
     });
+})
+
+.factory('cadastroDeFotos', function(recursoFoto, $q){
+    var service = {};
+
+    service.cadastrar = function(foto){
+        return $q(function(resolve, reject){
+            if(foto._id){
+                // edicao
+                recursoFoto.update({fotoId: foto._id}, foto, function(){
+                    resolve({
+                        mensagem : 'Foto ' + foto.titulo + ' atualizada com sucesso!!',
+                        inclusao : false
+                    });
+                }, function(erro){
+                    console.log(erro);
+                    reject({
+                        mensagem : 'Não foi possivel atualizar a foto ' + foto.titulo
+                    });
+                });
+            }else{
+                // inclusao
+                recursoFoto.save(foto, function(){
+                    resolve({
+                        mensagem : 'Foto ' + foto.titulo + ' incluida com sucesso!',
+                        inclusao : true
+                    });
+                }, function(erro){
+                    console.log(erro);
+                    reject({
+                        mensagem : 'Não foi possivel incluir a foto ' + foto.titulo
+                    });
+                });
+            }
+        });
+    };
+
+    return service;
 });
